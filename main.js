e22b6b1d89203693913912b686ecc52f5d498881
Caijialinxx <caijialinxx@foxmail.com>
2018-07-02 17:42:32 +0800

    添加项目经验描述和链接

--- main.js
+++ main.js
@@ -12,20 +12,39 @@ Wechat：137xxxx6136 | Github: https://github.com/Caijialinxx
 - 掌握 SaSS、LeSS 的使用
 
 ## 项目经验
-### Node.js 搭建在线聊天室
-Node.js Express
-- 描述
+### [动态简历](#)
+JavaScript CSS
+- 主要技术栈： JavaScript 、 CSS
+- 使用 JS 和 CSS 模拟一个 markdown 简历的制作过程
+- JS 的具体实现中使用到了大量了 Promise 对象
+- CSS 采用 Flex 布局
 
 ### To-Do List
 React
-- 描述达飞
+- 主要技术栈：React
+- [版本一](https://caijialinxx.github.io/todo-react/build/)：实现用户登录和注册功能，使用第三方服务 LeanCloud 实现数据库存储数据
+- [版本二](https://caijialinxx.github.io/todo/build/)：无登录注册功能，使用 localStorage 存储数据
+- 功能：增加新的 todo 、标记 todo 的完成状态、移动及删除已有 todo
+- 组件化思想：分离各个拥有独立功能的组件，使项目和代码结构清晰，
 
-### Canvas 画板
-- 描述
+### Node.js 搭建在线聊天室
+Node.js Express
+- 主要技术栈： Node.js 、 Express 框架
+- 功能：使用唯一昵称登录、添加好友、加入/创建群聊、发送消息（文本、表情、文件）等
 
-### 在线动态简历
+### [Canvas 画板](https://caijialinxx.github.io/jirengu-homework/canvas/)
 JavaScript CSS
-- 描述
+- 主要技术栈： JavaScript 、 CSS
+- 功能：改变笔触的颜色和粗细、橡皮擦、一键清空、保存至本地功能
+- 兼容触屏设备（触屏设备直接使用截屏功能）
+
+### [轮播动画](https://caijialinxx.github.io/jirengu-homework/slides/)
+jQuery CSS
+- 主要技术栈： jQuery 、 CSS
+- 功能：切换至上一张、切换至下一张、圆点导航、自动循环轮播、鼠标悬停暂定播放
+- jQuery 的操作（DOM 操作、鼠标事件）
+- “状态机”原理切换图片，呈现出轮播的效果
+
 
 ## 教育经历
 ### 广东第二师范学院
@@ -47,6 +66,7 @@ let code_ready = `/*
 body {
   background: #fff3dd;
 }
+/* 让 wrapper 里的元素 flex 布局 */
 .wrapper {
   width: 100%;
   min-width: 960px;
@@ -77,7 +97,7 @@ body {
 
 /* 现在开始写简历啦！ */
 
-/* 让代码框腾出一些的位置给我的简历 */
+/* 让代码框腾出一些的位置给我的简历框 */
 #code_body {
   width: 35%;
 }
@@ -106,7 +126,6 @@ let code_marked = `
 let code_resume = `
 /* 调整我的简历，让它变得再好看一点 */
 #paper {
-  font-family: MicrosoftYaHei;
   font-size: 14px;
   line-height: 24px;
 }
