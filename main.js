0e624f87e1cc39d853cee63ce6756ff8c3e24d44
unknown <Caijialinxx>
2021-01-31 20:20:36 +0800

    update 202101

--- main.js
+++ main.js
@@ -5,46 +5,25 @@ Blog: https://caijialinxx.github.io
 前端开发
 
 ## 专业技能
-- 掌握 HTML5 及 CSS3 ，熟悉传统布局、 Flex 布局、移动端适配等，有较好的审美及保证设计稿还原度。使用 Scss / Less 开发 CSS 
-- 掌握 JavaScript ，掌握原型、原型链、闭包、面向对象等。了解 ES6 部分新特性
-- 掌握 React 及其相关库的使用，理解生命周期、组件通信、异步更新、 Hooks 、虚拟 DOM 、 Diff 算法等
-- 使用 TypeScript 开发，理解接口、常用类型、可选链、空值合并运算符等
-- Vue 结合相关库的开发经验，掌握组件及组件间数据传递的使用、响应式原理、路由等
-- Git 的使用，代码仓库协同工作的能力、 npm 发布等经验
-- React Native 、原生小程序等移动端开发经验
-- 理解 MVC 、 MVVM 思想，了解 HTTP 基础
-- 使用 webpack 打包项目的经验，了解其资源管理的方法，如 css-loader 、 file-loader 等
-- 熟悉 jQuery 常用 API ，如 DOM 操作、特效、事件等。曾使用原生 JavaScript 封装简易版的 $.ajax()
-
-## 项目经验
-### CUI
-[Try it](https://caijialinxx.github.io/ui-demo/) | [View source](https://github.com/Caijialinxx/ui-demo)
-- 关键词：\`UI 组件库\`、\`React Hooks\`、\`TypeScript\`、\`npm\`、\`webpack\`、\`CircleCi\`
-- 描述：个人独立开发的 React UI 组件库，使用 React Hooks + TypeScript 开发，并已发布到 npm 中。由此掌握了 webpack 打包、 npm 发布流程、 CircleCi 自动化持续集成等。其中遇到的一个问题记录在了博客中——[从 npm 引入 React Hooks 轮子库报错 Minified React error#321 的解决方法](https://caijialinxx.github.io/2019/11/20/fix-react-error-321-by-webpack-externals/)。
-
-### 简历生成器
-[Try it](https://caijialinxx.github.io/animating-resume/) | [View source](https://github.com/Caijialinxx/animating-resume)
-- 关键词：\`JavaScript\`、\`Promise\`、\`变速\`、\`过渡动画\`、\`盒阴影\`
-- 描述：该项目实现将一个 Markdown 简历转成可下载的 PDF 简历。使用原生 JS 开发， Promise 处理异步链式操作（各个步骤的跳转衔接），利用 CSS3 实现过渡、动画、阴影和弹性布局等。
-
-### 炫主页
-[Try it](https://caijialinxx.github.io/resume/) | [View source](https://github.com/Caijialinxx/resume)
-- 关键词：\`jQuery\`、\`CSS3 过渡动画\`、\`组件化思想\`、\`MVC 思想\`、\`媒体查询\`、\`Sticky\`
-- 描述：此项目是本人的一个信息介绍页，包含基础信息、技能、项目展示以及留言板功能。有大量精美的动画，例如滚动时的导航栏及模块的浮起、导航栏 hover 动画、响应式设计等。
-
-### 幻灯片放映室
-[Try it](https://caijialinxx.github.io/Slides/) | [View source](https://github.com/Caijialinxx/Slides)
-- 关键词：\`jQuery\`、\`Loop模式\`、\`缩略图模式\`、\`Rem\`、\`适配移动端\`、\`ES6\`
-- 描述：本项目展示了两种不同风格的轮播，功能有上一张、下一张、可点击的原点/缩略图导航、自动循环播放。使用了 Rem 单位，以及适配移动端。
-
-### 涂鸦板
-[Try it](https://caijialinxx.github.io/Canvas/) | [View source](https://github.com/Caijialinxx/Canvas)
-- 关键词：\`HTML5\`、\`Canvas\`、\`ES6\`、\`JavaScript\`
-- 描述：使用 HTML5 的 Canvas 元素及其相关 API ，实现支持多指同时画图（可选色/粗细）、橡皮擦、一键清空、保存至本地的功能。
+- 掌握 HTML5 及 CSS3 ，熟悉传统布局、 Flex 布局、移动端适配等，有较好的审美及保证设计稿还原度。使⽤
+- Scss / Less 开发 CSS
+- 掌握 JavaScript ，掌握原型、原型链、闭包、⾯向对象等。了解 ES6 部分新特性
+- React 及其相关库的开发经验，理解⽣命周期、组件通信、异步更新、 Hooks 、虚拟 DOM 、 Diff 算法等
+- TypeScript 开发经验，理解接⼝、常⽤类型、可选链、空值合并运算符等
+- Vue 结合相关库的开发经验，掌握组件及组件间数据传递的使⽤、响应式原理、路由等
+- Git 的使⽤，代码仓库协同⼯作的能⼒、 npm 发布等经验
+- React Native 移动端开发经验
+- Webpack 打包项⽬的经验，了解常⽤的资源管理、插件、缓存等的使⽤
 
 ## 工作经历
+### 深圳市法本信息技术股份有限公司-平安寿险⼈⼒外包
+2020/5 - ⾄今
+- 职位：前端开发（含移动端和 PC 端）
+- 主要技术： React Native 、 Vue
+- ⼯作内容：⼀、在捷报 PC 项⽬中担任 Vue Web 开发，期间还负责代码审核及脚⼿架的升级，曾在⼩组内分享代码的优化建议，提⾼代码的可维护性和可扩展性。⼆、在捷报 App 项⽬中担任 React Native 开发，其中负责⼀些公共组件的开发，如兼容安卓及 IOS 系统的阴影组件，提供友好的 API 进⾏⾃适应或固定宽⾼及其他样式的定义。三、负责捷报 CMS 开发，曾为技术端的需求提供建议及分析。
+
 ### 深圳市亿兆互联技术有限公司
-2020/3 - 至今
+2020/3 - 2020/4
 - 职位：Web前端开发
 - 主要技术：\`Vue\`、\`Vue Router\`、\`Vuex\`
 - 工作内容：开发和维护智能照明控制系统，独立承担太阳能智能灯控系统中高德地图模块的开发。快速定位和解决 Bug ，针对可优化的地方主动提供优化方案并执行。
@@ -54,6 +33,27 @@ Blog: https://caijialinxx.github.io
 - 职位：Web前端开发
 - 主要技术：\`React\`、\`React Router\`、\`Mobx\`
 - 工作内容：开发智能英语学习平台。负责中后台 1.1 版本的所有更新和优化；To C 系统中负责 UI 组件的开发供前端调用，以及数学学习模块的更新维护。
+
+## 项目经验
+### CUI
+[预览](https://caijialinxx.github.io/ui-demo/#icon)
+- 关键词：\`UI 组件库\`、\`React Hooks\`、\`TypeScript\`、\`npm\`、\`webpack\`、\`CircleCi\`
+- 描述：个人独立开发的 React UI 组件库，使用 React Hooks + TypeScript 开发，并已发布到 npm 中。由此掌握了 webpack 打包、 npm 发布流程、 CircleCi 自动化持续集成等。其中遇到的一个问题记录在了博客中——[从 npm 引入 React Hooks 轮子库报错 Minified React error#321 的解决方法](https://caijialinxx.github.io/2019/11/20/fix-react-error-321-by-webpack-externals/)。
+
+### 简历生成器
+[预览](https://caijialinxx.github.io/animating-resume/)
+- 关键词：\`JavaScript\`、\`Promise\`、\`变速\`、\`过渡动画\`、\`盒阴影\`
+- 描述：该项目实现将一个 Markdown 简历转成可下载的 PDF 简历。使用原生 JS 开发， Promise 处理异步链式操作（各个步骤的跳转衔接），利用 CSS3 实现过渡、动画、阴影和弹性布局等。
+
+### 炫主页
+[预览](https://caijialinxx.github.io/resume/)
+- 关键词：\`jQuery\`、\`CSS3 过渡动画\`、\`组件化思想\`、\`MVC 思想\`、\`媒体查询\`、\`Sticky\`
+- 描述：此项目是本人的一个信息介绍页，包含基础信息、技能、项目展示以及留言板功能。有大量精美的动画，例如滚动时的导航栏及模块的浮起、导航栏 hover 动画、响应式设计等。
+
+### 涂鸦板
+[预览](https://caijialinxx.github.io/Canvas/)
+- 关键词：\`HTML5\`、\`Canvas\`、\`ES6\`、\`JavaScript\`
+- 描述：使用 HTML5 的 Canvas 元素及其相关 API ，实现支持多指同时画图（可选色/粗细）、橡皮擦、一键清空、保存至本地的功能。
 
 ## 教育经历
 ### 广东第二师范学院
@@ -337,7 +337,7 @@ function adjustResume() {
 }
 function structureResume() {
   $('#paper')[0].innerHTML = marked(resume)
-  $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="works"></div>', '<div id="jobs"></div>', '<div id="education"></div>')
+  $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="jobs"></div>', '<div id="works"></div>', '<div id="education"></div>')
   
   // 「基本信息」
   $('#information').append($('h1'), $('p').first())
